b815ce9ace97ca30dc6203f4076c0390ba5fd522
woodyx218 <woodyx218@gmail.com>
2018-07-20 09:18:26 -0400

    Add files via upload

--- COD collector.R
+++ COD collector.R
@@ -63,24 +63,34 @@ while (page_empty==0){
         style=tail(strsplit(address,'[.]')[[1]],1)
         # examine whether style is csv,xls,xlsx (prefix criterion is not strict)
         if (style=='xls' || style=='xlsx' || style=='csv'){
-          all_desc = c(all_desc,html_text(desc))
-          all_location = c(all_location,ccn(html_text(loca)))
-          all_date = c(all_date,html_text(year))
-          all_address=c(all_address,paste0("https://data.humdata.org",address0[i]))
-          download.file(addr,destfile = paste0("E:/File/Pennsylvania/UNFPA/data/",ccn(html_text(loca)),'_',html_text(year),'.',style))
+          loc=ccn(html_text(loca))
+          # examine whether location is in country list
+          if (!loc %in% country_list){
+            print(loc)
+          } else{
+            # there are two unusual cases: no date or has a period instead of single date
+            date=html_text(year)
+            # first examine non-empty date
+            if (nchar(date)>=12){
+              clean_date=str_sub(date,-12)
+              all_date = c(all_date,clean_date)
+              all_location = c(all_location,loc)
+              all_desc = c(all_desc,html_text(desc))
+              all_address=c(all_address,paste0("https://data.humdata.org",address0[i]))
+              download.file(addr,quiet = 1,destfile = paste0("E:/File/Pennsylvania/UNFPA/data/",loc,'_',clean_date,'.',style))
+            }
+          }
         }
       }
     }
   }
 }
-print(paste0('COD data processing takes',Sys.time()-start_time,'minutes'))
+print(paste0('COD data processing takes ',round(Sys.time()-start_time,2),' minutes'))
 
 
 
-
+# define a function that 'clean country name'(ccn), e.g. ccn("Côte d'Ivoire")
 ccn=function(string){
-  # define a function that 'clean country name'(ccn), e.g. ccn("Côte d'Ivoire")
-  
   # remove non-alphabetic
   string=str_replace_all(string, "[[:punct:]]", " ")
   # remove Latin letters
@@ -88,4 +98,38 @@ ccn=function(string){
 
 
 
-directory=data.frame(country=all_location,year=all_date,decription=all_desc,link=all_address)
+directory=data.frame(country=all_location,year=all_date,decription=all_desc,link=all_address,stringsAsFactors=FALSE)
+
+
+
+# build a country list from https://www.unfpa.org/worldwide
+country_list=c('Angola','Botswana','Burundi','Comoros','Democratic Republic of the Congo',
+               'Eritrea','Eswatini','Ethiopia','Kenya','Lesotho','Madagascar','Malawi','Mozambique',
+               'Namibia','Rwanda','Seychelles','South Africa','South Sudan',
+               'United Republic of Tanzania','Uganda','Zambia','Zimbabwe','Benin','Burkina Faso',
+               'Republic of Cameroon','Cabo Verde','Central African Republic','Chad',
+               'Republic of Congo','Cote d Ivoire','Equatorial Guinea','Gabon','Gambia','Ghana',
+               'Guinea','Guinea-Bissau','Liberia','Mali','Mauritania','Niger','Nigeria',
+               'Sao Tome and Principe','Senegal','Sierra Leone','Togo','Algeria','Djibouti',
+               'Egypt','Kingdom of Bahrain','Kingdom of Saudi Arabia','State of Kuwait',
+               'State of Qatar','United Arab Emirates','Sultanate of Oman','Iraq','Jordan','Lebanon',
+               'Libyan Arab Jamahiriya','Morocco','State of Palestine','Somalia','Sudan',
+               'Syrian Arab Republic','Tunisia','Yemen','Afghanistan','Bangladesh','Bhutan',
+               'Cambodia','China','Democratic Peoples Republic of Korea','India','Indonesia',
+               'Islamic Republic of Iran','Lao People s Democratic Republic','Malaysia','Maldives',
+               'Mongolia','Myanmar','Nepal','Cook Islands','Federated States of Micronesia',
+               'Fiji','Kiribati','Marshall Islands','Nauru','Niue','Palau','Samoa',
+               'Solomon Islands','Tokelau','Tonga','Tuvalu and Vanuatu','Pakistan',
+               'Papua New Guinea','Philippines','Sri Lanka','Thailand','Timor-Leste','Viet Nam',
+               'Albania','Armenia','Azerbaijan','Belarus','Bosnia & Herzegovina','Georgia',
+               'Kazakhstan','Kosovo','Kyrgyzstan','Macedonia','Moldova','Serbia','Tajikistan',
+               'Turkey','Turkmenistan','Ukraine','Uzbekistan','Argentina','Bolivia','Brazil',
+               ' Belize','Guyana','Saint Lucia','Jamaica','Suriname','Trinidad and Tobago',
+               'Anguilla','Antigua and Barbuda','Aruba','Bahamas','Barbados','Bermuda',
+               'British Virgin Islands','Cayman Islands','Dominica','Grenada','Montserrat',
+               'Netherlands Antilles','Saint Kitts and Nevis','Saint Vincent and the Grenadines',
+               'Turks and Caicos Islands','Chile','Colombia','Costa Rica','Cuba',
+               'Dominican Republic','Ecuador','El Salvador','Guatemala','Haiti','Honduras','Mexico',
+               'Nicaragua','Panama','Paraguay','Peru','Uruguay','Venezuela')
+
+
